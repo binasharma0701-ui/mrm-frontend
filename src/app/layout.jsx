fb4c418db5c1b33f1c618d1f1dba310cd9475d80
Navisharma579 <navartansharma41@gmail.com>
2026-03-28 09:58:29 +0530

    fix ui releted issues

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,13 +1,11 @@
 import Navbar from '../components/layout/Navbar/Navbar'
 import Footer from '../components/layout/Footer/Footer'
-import MobileMenu from '../components/layout/MobileMenu/MobileMenu'
 import '../styles/global.css'
 
 export default function Layout({ children }) {
   return (
     <div className="app-layout">
       <Navbar />
-      <MobileMenu />
       <main className="main-content">
         {children}
       </main>
